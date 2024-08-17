a7a71ff7910b2eacd185c7be1013c5af5f6c7296
David Turkalj <davidturkalj@gmail.com>
2024-08-18 00:37:38 +0200

    day4

---
+++ src/day4/taskTwo.ts
@@ -0,0 +1,9 @@
+import {createHash} from 'node:crypto';
+
+for (let i = 0; i < 10000000; i++) {
+    let hashed = createHash('md5').update("bgvyzdsv" + i).digest('hex');
+    if (hashed.startsWith("000000")) {
+        console.log(i);
+        break;
+    }
+}
