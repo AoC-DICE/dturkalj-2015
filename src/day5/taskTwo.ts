00dbd2eae4325b092b722b40f31e277fbf53a408
David Turkalj <davidturkalj@gmail.com>
2024-08-18 00:37:47 +0200

    day5

---
+++ src/day5/taskTwo.ts
@@ -0,0 +1,13 @@
+import * as fs from 'fs';
+
+let input: string = fs.readFileSync('src/day5/input.txt', "utf-8");
+let words = input.split("\n");
+let nice = 0;
+let bad = 0;
+let regex = new RegExp(/(?=.*([a-z]{2}).*\1)(?=.*([a-z]).\2)/);
+words.forEach((word, index) => {
+    if (regex.test(word)) nice++;
+    else bad++;
+});
+
+console.log(`Nice: ${nice}, bad: ${bad}`);
